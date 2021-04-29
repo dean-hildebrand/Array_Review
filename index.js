571ffa43dc0c0ef0416f5e101135840e39e3f505
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-04-29 12:14:07 -0400

    Array practice with methods

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-alert("connected");
+// let array1 = [1, 2, , 3, 4, 5];
+
+// let found = array1.find((elem) => {
+//   return elem > 4;
+// });
+
+// const sum = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue
+// );
+
+// console.log(sum);
+
+//-------------- Array From Range ----------------------------
+
+// const number = arrayFromRange(-1, 4);
+
+// function arrayFromRange(min, max) {
+//   let arr = [];
+//   for (let i = min; i <= max; i++) arr.push(i);
+//   return arr;
+// }
+
+// console.log(number);
+
+//------------------ Includes ---------------------------
+
+const numbers = [2, 3, 5];
+
+console.log(includes(numbers, 5));
+
+function includes(array, searchElement) {
+  for (let element of array) {
+    if (element === searchElement) {
+      return true;
+    }
+    return false;
+  }
+}
